9c00ffe3a917b71e6b8c14f29c287de0bfd63c38
erioz3802 <erioz3802@yahoo.com>
2025-09-26 13:54:43 -0500

    Update app.run host and port configuration
    Change app.run to use 0.0.0.0 and environment PORT

--- app.py
+++ app.py
@@ -802,4 +802,4 @@ if __name__ == '__main__':
     print(f"- ✅ Official assignment system")
        
      
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
